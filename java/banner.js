f8658424936ea0ac86567362abde6069115ee396
Erfan Khan <erfan.khan.cse@gmail.com>
2025-03-02 05:38:39 +0600

    Changing Background function

--- java/banner.js
+++ java/banner.js
@@ -4,3 +4,26 @@ document.addEventListener("DOMContentLoaded", function () {
   settingInnerText("week", timeDate[0]);
   settingInnerText("date", timeDate[1]);
 });
+
+// background color changer
+
+function getRandomColor() {
+  const colors = [
+    "rgba(255, 0, 0, 0.3)",
+    "rgba(255, 165, 0, 0.3)",
+    "rgba(255, 255, 0, 0.3)",
+    "rgba(0, 128, 0, 0.3)",
+    "rgba(0, 0, 255, 0.3)",
+    "rgba(75, 0, 130, 0.3)",
+    "rgba(238, 130, 238, 0.3)",
+  ];
+
+  const randomIndex = Math.floor(Math.random() * colors.length);
+  return colors[randomIndex];
+}
+
+document
+  .getElementById("color_change_btn")
+  .addEventListener("click", function () {
+    document.body.style.backgroundColor = getRandomColor();
+  });
